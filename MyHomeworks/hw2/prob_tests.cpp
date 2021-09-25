237aee1f8aa0d9f0c64bf4298c5a4f7a589e6216
korliakov <korliakov.dk@phystech.edu>
2021-09-25 09:55:28 +0300

    Changed class SegmentModState

--- MyHomeworks/hw2/prob_tests.cpp
+++ MyHomeworks/hw2/prob_tests.cpp
@@ -41,22 +41,6 @@ public:
 };
 
 
-class SegmentModState : public State {
-private:
-    int const beg, end;
-
-    std::set<int> const incl;
-    std::set<int> const excl;
-
-
-public:
-    SegmentModState(): beg(0), end(-1) { }
-    SegmentModState(int beg, int end, std::set<int> const &inc, std::set<int> const &exc ): beg(beg), end(end), incl(inc), excl(exc)  { }
-    
-    virtual bool contains(int s) const {
-        return ((s >= beg && s <= end) || (incl.count(s) > 0)) && (excl.count(s) == 0) ;
-    }
-};
 
 class SetState : public State {
 private:
@@ -70,6 +54,26 @@ public:
         return states.count(s) > 0;
     }
 };
+
+
+
+class SegmentModState : public State {
+private:
+    SegmentState const segstate;
+
+    SetState const incl;
+    SetState const excl;
+
+
+public:
+    SegmentModState(int a, int b, std::set<int> const &inc, std::set<int> const &exc ): segstate(a, b), incl(inc), excl(exc)  { }
+    
+    virtual bool contains(int s) const {
+        return (segstate.contains(s) || (incl.contains(s))) && (!excl.contains(s)) ;
+    }
+};
+
+
 
 class ProbabilityTest {
 private:
@@ -104,33 +108,38 @@ int main(int argc, const char * argv[]) {
     DiscreteState d(5);
     SegmentState s(0,4);
     SetState ss({1, 3, 5, 7, 23, 48, 57, 60, 90, 99});
-    
 
-    std::ofstream out;          
-    out.open("Discrete2.txt");
-    for (int i = 1; i < 101; i+=1) {
-        ProbabilityTest pt(9,0,i, 10000);
-        out << i << " , " << pt(d) << std::endl;
-    }
-    out.close();
-    
-    std::cout << "1 тест готов" << std::endl;
 
-    out.open("Segment2.txt");
-    for (int i = 1; i < 101; i+=1) {
-        ProbabilityTest pt(9 ,0,i, 10000);
+    SegmentModState sms(0, 10, {1,2}, {20, 22});
+    
+    ProbabilityTest pt(9,0,100, 100000);
+    
+    std::cout << pt(sms) << std::endl;
+  //  std::ofstream out;          
+    //out.open("Discrete2.txt");
+    //for (int i = 1; i < 101; i+=1) {
+     //   ProbabilityTest pt(9,0,i, 10000);
+      //  out << i << " , " << pt(d) << std::endl;
+    //}
+    //out.close();
+    
+    //std::cout << "1 тест готов" << std::endl;
+
+    //out.open("Segment2.txt");
+    //for (int i = 1; i < 101; i+=1) {
+      //  ProbabilityTest pt(9 ,0,i, 10000);
         
-        out << i << " , " << pt(s) << std::endl;
-    }
-    out.close();
-    std::cout << "2 тест готов" << std::endl;
-    out.open("Set2.txt");
-    for (int i = 1; i < 101; i+=1) {
-        ProbabilityTest pt(9 ,0,i, 10000);
-        out << i << " , " << pt(ss) << std::endl;
-    }
-    out.close();
-    std::cout << "3 тест готов" << std::endl; 
+        //out << i << " , " << pt(s) << std::endl;
+    //}
+    //out.close();
+    //std::cout << "2 тест готов" << std::endl;
+    //out.open("Set2.txt");
+    //for (int i = 1; i < 101; i+=1) {
+      //  ProbabilityTest pt(9 ,0,i, 10000);
+        //out << i << " , " << pt(ss) << std::endl;
+    //}
+    //out.close();
+    //std::cout << "3 тест готов" << std::endl; 
 
 
     return 0;
